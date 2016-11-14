ce90e7c89761f83f1c16f18b71971823f23d6f77
Ethan B <ethanbinya@aol.com>
2016-11-13 22:03:59 -0500

    added working project

---
+++ CppWinForm1/MyForm.cpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "MyForm.h"
+#include<fstream>
+#include"Quotes.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	CppWinForm1::MyForm form;
+	Application::Run(%form);
+}
